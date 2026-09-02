2fc9190c1ef12a3bff91770b701bb3ab1961a79d
Hgregori <Hgregorio@byupathway.edu>
2026-09-01 21:00:36 -0300

    W01-IApt2

---
+++ db.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+async function dataConnection() {
+    const uri = "mongodb://demo:demo123@ac-xvu3hrn-shard-00-00.skbcv6a.mongodb.net:27017,ac-xvu3hrn-shard-00-01.skbcv6a.mongodb.net:27017,ac-xvu3hrn-shard-00-02.skbcv6a.mongodb.net:27017/?ssl=true&replicaSet=atlas-1trrzt-shard-0&authSource=admin&appName=Cluster0";
+
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    } finally {
+        await client.close();
+        console.log("MongoDB connection closed");
+    }
+}
+
+dataConnection();

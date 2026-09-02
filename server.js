6376a9543ffcb7f134e55699a1690d1132e81cfe
Hgregori <Hgregorio@byupathway.edu>
2026-09-02 17:53:50 -0300

    W01-PPpt1

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/professional', (req, res) => {
-    res.json({
-        professionalName: 'Henrique Missieri',
-        base64Image: '',
-        nameLink: {
-            firstName: 'Henrique',
-            url: 'https://github.com'
-        },
-        primaryDescription: 'Software Development Student',
-        workDescription1: 'Aprendendo Express.js',
-        workDescription2: 'Aprendendo APIs REST',
-        linkTitleText: 'Links',
-        linkedInLink: {
-            text: 'LinkedIn',
-            link: 'https://linkedin.com'
-        },
-        githubLink: {
-            text: 'GitHub',
-            link: 'https://github.com'
-        }
-    });
-});
+app.use('/', require('./routes/index.js'));
 
 app.listen(PORT, () => {
-    console.log("Web server is running on port " + PORT);
+    console.log(`Server is running on port ${PORT}`);   
 });
